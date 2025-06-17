422491b141f77571186d369c342696a556b85146
Matt Young <31349794+mattgoespro@users.noreply.github.com>
2025-06-17 10:12:50 +0200

    cp

--- src/renderer/react-router.config.ts
+++ src/renderer/react-router.config.ts
@@ -1,11 +1,7 @@
-// src/renderer/react-router.config.ts
-import path from "path";
 import type { Config } from "@react-router/dev/config";
-
-// This resolves to: <project-root>/src/renderer/app
-const appDir = path.resolve(__dirname, "app");
+import path from "path";
 
 export default {
   ssr: false, // SPA mode
-  appDirectory: appDir // Absolute path to your app dir
+  appDirectory: path.resolve(__dirname, "app")
 } satisfies Config;
